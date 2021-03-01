2fd5d60c54d7a26c819fd5ce0814cc2a645707e7
wiltonOA <gr.visaoconsultoriaemti@gmail.com>
2021-02-28 22:00:55 -0300

    Subindo projeto inicial

---
+++ src/test/java/br/com/visaosoftware/brasileiraoapi/BrasileiraoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.visaosoftware.brasileiraoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
