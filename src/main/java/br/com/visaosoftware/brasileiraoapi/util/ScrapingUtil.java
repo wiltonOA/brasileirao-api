be7d53f966f78ae3efcd3000794e1869cbbbca35
wiltonOA <gr.visaoconsultoriaemti@gmail.com>
2021-02-28 23:27:12 -0300

    Conectando na pagina do google com Jsoup

--- src/main/java/br/com/visaosoftware/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/visaosoftware/brasileiraoapi/util/ScrapingUtil.java
@@ -1,10 +1,43 @@
 package br.com.visaosoftware.brasileiraoapi.util;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import br.com.visaosoftware.brasileiraoapi.dto.PartidaGoogleDTO;
+
+
 public class ScrapingUtil {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScrapingUtil.class);
+	
+	private static final String BASE_URL_GOOGLE = "https://www.google.com.br/search?q=";
+	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url = BASE_URL_GOOGLE+"palmeiras+x+corinthians+08/08/2020"+COMPLEMENTO_URL_GOOGLE;
+		
+		ScrapingUtil scraping = new ScrapingUtil();
+		scraping.obtemInformacoesPartida(url);
 
+	}
+	
+	public PartidaGoogleDTO obtemInformacoesPartida(String url) {
+		PartidaGoogleDTO partida = new PartidaGoogleDTO();
+		
+		Document document = null;
+		
+		try {
+			document = Jsoup.connect(url).get();
+			
+			String titulo = document.title();
+			LOGGER.info("Titulo da pagina ->{}", titulo);
+			
+		}catch(Exception ex) {
+			LOGGER.error("ERRO AO CONECTAR NA PAGINA DO GOOGLE COM JSOUP ->{}", ex.getMessage());
+		}
+		
+		return partida;
 	}
 
 }
