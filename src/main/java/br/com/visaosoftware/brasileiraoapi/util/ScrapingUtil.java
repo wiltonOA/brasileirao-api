1f7efb282e3eb7f4179a714a370c5badb025bd74
wiltonOA <gr.visaoconsultoriaemti@gmail.com>
2021-03-02 00:00:57 -0300

    Recuperando o nome das equipes e suas logos

--- src/main/java/br/com/visaosoftware/brasileiraoapi/util/ScrapingUtil.java
+++ src/main/java/br/com/visaosoftware/brasileiraoapi/util/ScrapingUtil.java
@@ -2,6 +2,7 @@ package br.com.visaosoftware.brasileiraoapi.util;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,10 +35,24 @@ public class ScrapingUtil {
 			LOGGER.info("Titulo da pagina ->{}", titulo);
 
 			StatusPartida statusPartida = obtemStatusDaPartida(document);
-			LOGGER.info("Status da partida ->{}",statusPartida.toString());
+			LOGGER.info("Status da partida ->{}", statusPartida.toString());
+
+			if (statusPartida != StatusPartida.PARTINA_NAO_INICIADA) {
+				String tempoPartida = obtemTempoPartida(document);
+				LOGGER.info("Tempo da partida ->{}", tempoPartida);
+			}
+
+			String nomeEquipeCasa = recuperaNomeEquipeCasa(document);
+			LOGGER.info("Equipe casa ->{}", nomeEquipeCasa);
+
+			String nomeEquipeVisitante = recuperaNomeEquipeVisitante(document);
+			LOGGER.info("Equipe casa ->{}", nomeEquipeVisitante);
+
+			String urlLogoEquipeCasa = recuperaLogoEquipeCasa(document);
+			LOGGER.info("URL logo equipe casa ->{}", urlLogoEquipeCasa);
 			
-			String tempoPartida = obtemTempoPartida(document);
-			LOGGER.info("Tempo da partida ->{}",tempoPartida);
+			String urlLogoEquipeVisitante = recuperaLogoEquipeVisitante(document);
+			LOGGER.info("URL logo equipe visitante ->{}", urlLogoEquipeVisitante);
 
 		} catch (Exception ex) {
 			LOGGER.error("ERRO AO CONECTAR NA PAGINA DO GOOGLE COM JSOUP ->{}", ex.getMessage());
@@ -62,7 +77,6 @@ public class ScrapingUtil {
 		if (!isTempoPartida) {
 			statusPartida = StatusPartida.PARTIDA_ENCERRADA;
 		}
-		
 
 		return statusPartida;
 	}
@@ -70,21 +84,56 @@ public class ScrapingUtil {
 	// Metodo que retorna o tempo da partida
 	public String obtemTempoPartida(Document document) {
 		String tempoPartida = "";
-		//Jogo em andamento ou intervalo ou em penalidades
+		// Jogo em andamento ou intervalo ou em penalidades
 		boolean isTempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").isEmpty();
 		if (!isTempoPartida) {
 			tempoPartida = document.select("div[class=imso_mh__lv-m-stts-cont]").first().text();
 		}
-		//Jogo encerrado
+		// Jogo encerrado
 		isTempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").isEmpty();
 		if (!isTempoPartida) {
-			tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first().text();
+			tempoPartida = document.select("span[class=imso_mh__ft-mtch imso-medium-font imso_mh__ft-mtchc]").first()
+					.text();
 		}
-		//Formatando o temo
-		if(tempoPartida.contains("'")) {
+		// Formatando o temo
+		if (tempoPartida.contains("'")) {
 			tempoPartida = tempoPartida.replace("'", " min");
 		}
 		return tempoPartida;
+	}
+
+	public String recuperaNomeEquipeCasa(Document document) {
+		String nomeEqupeCasa = "";
+		Element elemento = document.selectFirst("div[class=imso_mh__first-tn-ed imso_mh__tnal-cont imso-tnol]");
+
+		nomeEqupeCasa = elemento.select("span").text();
+
+		return nomeEqupeCasa;
+	}
+
+	public String recuperaNomeEquipeVisitante(Document document) {
+		String nomeEqupeVisitante = "";
+		Element elemento = document.selectFirst("div[class=imso_mh__second-tn-ed imso_mh__tnal-cont imso-tnol]");
+
+		nomeEqupeVisitante = elemento.select("span").text();
+
+		return nomeEqupeVisitante;
+	}
+
+	public String recuperaLogoEquipeCasa(Document document) {
+
+		Element elemento = document.selectFirst("div[class=imso_mh__first-tn-ed imso_mh__tnal-cont imso-tnol]");
+		String urlLogoEquipeCasa = "https://" + elemento.select("img[class=imso_btl__mh-logo]").attr("src");
+
+		return urlLogoEquipeCasa;
+	}
+
+	public String recuperaLogoEquipeVisitante(Document document) {
+
+		Element elemento = document.selectFirst("div[class=imso_mh__second-tn-ed imso_mh__tnal-cont imso-tnol]");
+		String urlLogoEquipeVisitante = "https://" + elemento.select("img[class=imso_btl__mh-logo]").attr("src");
+
+		return urlLogoEquipeVisitante;
 	}
 
 }
